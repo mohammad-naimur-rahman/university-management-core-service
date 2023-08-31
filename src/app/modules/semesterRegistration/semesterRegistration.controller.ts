8765b4117488f77900e16bce1930f0e0445db413
Naimur <naeemhasan28@gmail.com>
2023-08-31 10:11:50 +0600

    add: student semester registration

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import catchAsync from '../../../shared/catchAsync';
@@ -68,10 +69,24 @@ const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const startMyRegistration = catchAsync(async (req: Request, res: Response) => {
+  const user = (req as any).user;
+  const result = await SemesterRegistrationService.startMyRegistration(
+    user.userId
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student SemesterRegistration started successfully',
+    data: result,
+  });
+});
+
 export const SemesterRegistrationController = {
   insertIntoDB,
   getAllFromDB,
   getByIdFromDB,
   updateOneInDB,
   deleteByIdFromDB,
+  startMyRegistration,
 };
--- src/shared/utils.ts
+++ src/shared/utils.ts
@@ -1,3 +1,5 @@
+import { WeekDays } from '@prisma/client';
+
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export const asyncForEach = async <T>(arr: Array<T>, cb: any) => {
   if (!Array.isArray(arr)) {
@@ -6,4 +8,29 @@ export const asyncForEach = async <T>(arr: Array<T>, cb: any) => {
   for (let index = 0; index < arr.length; index++) {
     await cb(arr[index], index, arr);
   }
+};
+
+export const hasTimeConflict = (
+  existingSlots: {
+    startTime: string;
+    endTime: string;
+    dayOfWeek: WeekDays;
+  }[],
+  newSlot: {
+    startTime: string;
+    endTime: string;
+    dayOfWeek: WeekDays;
+  }
+) => {
+  for (const slot of existingSlots) {
+    const existingStart = new Date(`1970-01-01T${slot.startTime}:00`);
+    const existingEnd = new Date(`1970-01-01T${slot.endTime}:00`);
+    const newStart = new Date(`1970-01-01T${newSlot.startTime}:00`);
+    const newEnd = new Date(`1970-01-01T${newSlot.endTime}:00`);
+
+    if (newStart < existingEnd && newEnd > existingStart) {
+      return true;
+    }
+  }
+  return false;
 };
